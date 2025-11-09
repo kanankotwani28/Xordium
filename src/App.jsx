f2d8dd7f11180a4b060f68717e2a2c42c6a2507f
Kanan Kotwani <kanankotwani28@gmail.com>
2025-11-09 13:53:30 +0530

    hero bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,20 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Trusted from "./components/Trusted";
 import Features from "./components/Features";
-import VerifyFeatures from "./components/VerifyFeatures.jsx";
 import Clients from "./components/Clients";
 import Pricing from "./components/Pricing";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
-import Section from "./components/Section";
 
 export default function App() {
   return (
-    <div className="min-h-screen text-gray-100 antialiased">
-      {/* <main className="px-6 md:px-12 lg:px-24"> */}
-      <main>
-        <Section className="grid grid-rows-[auto,1fr]">
-          <Navbar />
-          <Hero />
-        </Section>
+    <div className="min-h-screen bg-[#07060a] text-gray-100 antialiased">
+      <Navbar />
+      <main className="px-6 md:px-12 lg:px-24">
+        <Hero />
         <Trusted />
         <Features />
-        <VerifyFeatures />
-        {/* <Clients /> */}
+        <Clients />
         <Pricing />
         <CTA />
       </main>
